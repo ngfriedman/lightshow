41ae0c55f150b165e4ccb26c41c1608a3391c9bf
Timothy Stiles <tim@stiles.io>
2017-12-19 15:32:55 -0500

    initial commit.

---
+++ main.py
@@ -0,0 +1,8 @@
+import twitter
+api = twitter.Api(consumer_key=['2NaQG8gvhJFQMBqBFSUMtmNXU'],
+                  consumer_secret=['x40pfiGb10mh5O9YrBNRf0sONjKZlRNHjwzcYe7cFmrjWO7MBn'],
+                  access_token_key=['943212098947084288-PqVPedPoMTMyDJVANMMadC1BuKshDNJ'],
+                  access_token_secret=['BVSEfLcE8wvcu9FEOwNwOHqgv35EKvx1Twtt6KLa7G2nr'])
+
+print(api)
+

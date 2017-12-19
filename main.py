ae1b539362aa98e84a19f88fa7af214687e941ce
Timothy Stiles <tim@stiles.io>
2017-12-19 15:49:10 -0500

    it tweets

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from __future__ import unicode_literals
 import twitter
-api = twitter.Api(consumer_key=['2NaQG8gvhJFQMBqBFSUMtmNXU'],
-                  consumer_secret=['x40pfiGb10mh5O9YrBNRf0sONjKZlRNHjwzcYe7cFmrjWO7MBn'],
-                  access_token_key=['943212098947084288-PqVPedPoMTMyDJVANMMadC1BuKshDNJ'],
-                  access_token_secret=['BVSEfLcE8wvcu9FEOwNwOHqgv35EKvx1Twtt6KLa7G2nr'])
+api = twitter.Api(consumer_key='2NaQG8gvhJFQMBqBFSUMtmNXU',
+                  consumer_secret='x40pfiGb10mh5O9YrBNRf0sONjKZlRNHjwzcYe7cFmrjWO7MBn',
+                  access_token_key='943212098947084288-PqVPedPoMTMyDJVANMMadC1BuKshDNJ',
+                  access_token_secret='BVSEfLcE8wvcu9FEOwNwOHqgv35EKvx1Twtt6KLa7G2nr')
 
 print(api)
-
+status = api.PostUpdate('I love python-twitter!')
+status = api.PostUpdate('These are lights!')
+# print(status.text)
